3957e5cecfe721f0ea5ff0cd1b7f606b00d8409b
kor-Chipmunk <rhj4862@gmail.com>
2018-07-01 05:59:41 +0900

    Apply Design For Calendar & DetailView

--- Check-mate/views/calendar/CalendarDetailViewController.swift
+++ Check-mate/views/calendar/CalendarDetailViewController.swift
@@ -35,38 +35,60 @@ class CalendarDetailViewController: UIViewController {
     
 }
 
-internal class TotalMoneyHeaderCell: UITableViewCell {
+internal class TodayMoneyCell: UITableViewCell {
     
-    @IBOutlet weak var lblHeader: UILabel!
+    @IBOutlet weak var lblMoney: UILabel!
     
 }
 
-internal class TotalMoneyCell: UITableViewCell {
+internal class TodayTimeCell: UITableViewCell {
     
-    @IBOutlet weak var lblTotalMoney: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    
+}
+
+internal class EtcCell: UITableViewCell {
+    
+    @IBOutlet weak var lblTitle1: UILabel!
+    @IBOutlet weak var lblTitle2: UILabel!
+    @IBOutlet weak var lblTitle3: UILabel!
+    @IBOutlet weak var lblTitle4: UILabel!
+    
+    @IBOutlet weak var lblPay1: UILabel!
+    @IBOutlet weak var lblPay2: UILabel!
+    @IBOutlet weak var lblPay3: UILabel!
+    @IBOutlet weak var lblPay4: UILabel!
+    
     
 }
 
 extension CalendarDetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if indexPath.row == 0 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "lblTotalMoneyHeader", for: indexPath) as? TotalMoneyHeaderCell else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "todayMoneyCell", for: indexPath) as? TodayMoneyCell else {
+                return UITableViewCell()
+            }
+            
+            cell.lblMoney.text = "\(Price) won"
+            
+            return cell
+        }
+        else if indexPath.row == 1 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "todayTimeCell", for: indexPath) as? TodayTimeCell else {
                 return UITableViewCell()
             }
             
             return cell
         }
-        else if indexPath.row == 1 {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "lblMoney", for: indexPath) as? TotalMoneyCell else {
+        else if indexPath.row == 2 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "etcCell", for: indexPath) as? EtcCell else {
                 return UITableViewCell()
             }
-            
-            cell.lblTotalMoney.text = "\(Price) 원"
             
             return cell
         }
@@ -76,10 +98,28 @@ extension CalendarDetailViewController: UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
-            return 80.0
+            return 74.0 + 10.0
+        }
+        else if indexPath.row == 1 {
+            return 146.0 + 10.0
+        }
+        else if indexPath.row == 2 {
+            return 247.0 + 10.0
         }
         
         return 60.0
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if indexPath.row < 2 {
+            let screenSize = UIScreen.main.bounds
+            let separatorHeight = CGFloat(10.0)
+            
+            let additionalSeparator = UIView.init(frame: CGRect(x: 0, y: cell.frame.size.height-separatorHeight, width: screenSize.width, height: separatorHeight))
+            additionalSeparator.backgroundColor = UIColor.init(red: 244/250, green: 244/250, blue: 244/250, alpha: 1)
+            cell.addSubview(additionalSeparator)
+        }
     }
     
 }

6dc0efe1eea5bbbd5a0f0ca488ef926299f7663d
Noverish Harold <embrapers263@gmail.com>
2018-07-01 00:07:45 +0900

    Added WorkSpaceCardTableView

---
+++ Check-mate/views/workspace/WorkSpaceView.swift
@@ -0,0 +1,42 @@
+//
+//  WorkSpaceView.swift
+//  Check-mate
+//
+//  Created by Noverish Harold on 2018. 6. 30..
+//  Copyright © 2018년 MashUp. All rights reserved.
+//
+
+import UIKit
+
+class WorkSpaceView: UIView {
+
+    @IBOutlet weak var rootView: UIView!
+    @IBOutlet weak var addBtn: UIButton!
+
+    private var mIsLastCardView = false
+    var isLastCardView: Bool {
+        get {
+            return mIsLastCardView
+        }
+        set(b) {
+            mIsLastCardView = b
+            addBtn.isHidden = !mIsLastCardView
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    private func setup() {
+        self.loadNib(String(describing: WorkSpaceView.self))
+
+        rootView.layer.cornerRadius = 10
+    }
+}

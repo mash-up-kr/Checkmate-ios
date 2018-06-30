f333d8e13d7c99f22df101be57c4e28beb8f10ef
Noverish Harold <embrapers263@gmail.com>
2018-06-30 19:28:58 +0900

    Created Xcode Project

---
+++ Check-mate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check-mate
+//
+//  Created by Noverish Harold on 2018. 6. 30..
+//  Copyright © 2018년 MashUp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
